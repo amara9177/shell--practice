66ec4ddabf0171262ae098e8dc035c6ddc62c891
amara <rama@gmail.com>
2025-10-05 09:41:35 +0530

    some message

--- 11-exit status.sh
+++ 11-exit status.sh
@@ -4,12 +4,14 @@ USERID=$( id -u )
 
 if [ $USERID -ne 0 ];then
     echo "ERROR:please run the script with root privilege"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ];then
     echo "ERROR:installing mysql is Failure"
+    exit 1
 else
     echo "ERROR:installing mysql is Success"
 fi
