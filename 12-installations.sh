72c83d9e8f1594148118538bfa9f068965622828
amara <rama@gmail.com>
2025-10-05 11:08:12 +0530

    some message

---
+++ 12-installations.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$( id -u )
+
+if [ $USERID -ne 0 ];then
+    echo "ERROR:please run the script with root privilege"
+    exit 1
+fi
+
+VALIDATE(){
+if [ $1 -ne 0 ];then
+    echo "ERROR:installing $2 is Failure"
+    exit 1
+else
+    echo "ERROR:installing $2 is Success"
+fi
+}
+
+dnf install mysql -y
+validate $? "my sql"
+
+dnf install nginx -y
+validate $? "nginx"
+
+dnf install mangodb-mangosh -y
+validate $? "mangosh"
