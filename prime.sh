038e5754f493b7c15cffb3672f57fc2871adde5c
amara <rama@gmail.com>
2025-10-06 14:27:31 +0530

    some message

--- prime.sh
+++ prime.sh
@@ -3,7 +3,7 @@
 echo "please enter the number"
 read
 
-if [ $(($NUMBER -lt 2)) -eq 0 ];then
+if [ $(($NUMBER -lt 2)) ];then
     echo "Given number is prime number"
 else 
     echo "Given number is not prime number"
