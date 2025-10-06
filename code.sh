24d0756854e9192fe2af6018aabf9315066a21b2
amara <rama@gmail.com>
2025-10-06 18:37:36 +0530

    some message

---
+++ code.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ];then
+    echo  "ERROR:: please run script with root user privilege"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];then
+        echo " installing $2...$R Failure $N"
+        exit 1
+    else
+        echo  " installation $2...$G Success $N"
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ];then
+    dnf install mysql -y
+    VALIDATE $? "Mysql"
+else
+    echo "Mysql alreasy exist.....$Y skipping $N"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ];then
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo "Nginx alreasy exist.....$Y skipping $N"
+fi
+
+dnf list installed python03
+    if [ $? -ne 0 ];then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "Nginx alreasy exist.....$Y skipping $N"
+fi
