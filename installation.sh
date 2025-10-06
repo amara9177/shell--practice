2ac2033a90f03f2c4225e93be80a4c8ca8229ea4
amara <rama@gmail.com>
2025-10-06 15:21:43 +0530

    some message

--- installation.sh
+++ installation.sh
@@ -4,13 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ];then
     echo "ERROR: please run script with root user privilege"
-    read
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ];then
     echo "installation is Failure"
+    exit 1
 else
     echo "installation is Success"
 fi
